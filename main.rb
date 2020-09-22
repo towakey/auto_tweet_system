c44eb0921afb53de9a9118041b4ac4f5f1c722f8
towakey <kyosgame@gmail.com>
2020-09-23 02:16:32 +0900

    dotenv対応

--- main.rb
+++ main.rb
@@ -1,25 +1,25 @@
 require "twitter"
 require "date"
 require "json"
+require "dotenv"
 
 class TwitterClient
     attr_reader :client
     def initialize()
+        Dotenv.load(
+            File.join(File.dirname(File.expand_path(__FILE__)), '.env')
+        )
         @client = Twitter::REST::Client.new do |config|
-            config.consumer_key         = ""
-            config.consumer_secret      = ""
-            config.access_token         = ""
-            config.access_token_secret  = ""
+            config.consumer_key         = ENV["CONSUMER_KEY"]
+            config.consumer_secret      = ENV["CONSUMER_SECRET"]
+            config.access_token         = ENV["ACCESS_TOKEN"]
+            config.access_token_secret  = ENV["ACCESS_TOKEN_SECRET"]
         end
     end
     def tweet(str)
         @client.update(str)
     end
     def tweet_with_media(str,media_url)
-        # p media_url
-        # images = []
-        # images << File.new(media_url)
-        # @client.update_with_media(str,images)
         @client.update_with_media(str,media_url)
     end
     def get_current_time_str
